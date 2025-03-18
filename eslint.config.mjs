4f4cbecb8be25aeb577a3fe1ea9c1b6614ea323a
Mayis <saroyan.mayis.ait@gmail.com>
2025-03-18 15:40:29 +0400

    feat: install eslint prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,35 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      semi: ["warn"],
+      "no-console": ["warn"],
+      quotes: [
+        "error",
+        "double",
+        { avoidEscape: true, allowTemplateLiterals: true },
+      ],
+      "no-extra-boolean-cast": ["off"],
+      "no-empty-pattern": ["warn"],
+      "react-hooks/rules-of-hooks": ["error"],
+      "react-hooks/exhaustive-deps": ["warn"],
+      "react/prop-types": ["off"],
+      "no-unused-vars": ["off"],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": ["off"],
+      "@typescript-eslint/no-non-null-assertion": ["off"],
+      "no-empty-pattern": ["warn"],
+    },
+  }),
 ];
 
 export default eslintConfig;
